b85a5a2d4566fd8fd306b2466d5fa746bc4059d5
paulina-zajac <zajac-paulina@outlook.com>
2021-08-01 18:29:58 +0200

    add logout view

--- birdsite/birdnotes/urls.py
+++ birdsite/birdnotes/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('edit_observation/<int:id>/', views.edit_observation, name='edit_observation'),
     path('remove_observation/<int:id>/', views.remove_observation, name='remove_observation'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
