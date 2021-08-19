cc4262152a9c442cd2d05144e17e07276c485396
paulina-zajac <zajac-paulina@outlook.com>
2021-08-19 12:29:44 +0200

    remove key

--- birdsite/birdsite/settings.py
+++ birdsite/birdsite/settings.py
@@ -12,17 +12,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-##en9@-ki42=4#n16k)qb(ky9fn4$8trb=zy6xd@xqx-d-#(dp'
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
